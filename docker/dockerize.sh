055626aff14e150911b8143fe6b9e12858cd7c8f
Ilkka Veima <ilkka.veima@gmail.com>
2019-01-19 17:05:20 +0800

    Change tag to veimis (from codebunker)

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -2,7 +2,7 @@
 NAME=$(basename $(dirname `pwd`))
 
 docker build \
-    -t coderbunker/$NAME \
-    -t coderbunker/$NAME:`date +%Y-%m-%d` \
+    -t veimis/$NAME \
+    -t veimis/$NAME:`date +%Y-%m-%d` \
     -f ./Dockerfile \
     ..
